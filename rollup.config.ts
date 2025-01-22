e40d32b71492538b80f5ad3ba8880df2b3de7fcd
secret <secretx500@outlook.com>
2025-01-22 11:47:58 +0800

    refactor: 重构 Rollup 配置文件

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,12 +1,15 @@
 /** @type {import('rollup').RollupOptions} */
 
 import typescript2 from "rollup-plugin-typescript2";
+import terser from "@rollup/plugin-terser"; // 压缩代码插件
 
 export default {
   input: "./src/index.ts",
   output: {
     file: "./bin/index.js",
     format: "esm",
+    plugins: [terser()],
   },
+  external: ["sharp", "mockjs"],
   plugins: [typescript2()],
 };
